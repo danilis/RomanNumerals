d164c677a6f4b77da2b46fddbf853029d1f42bf5
danilis <danilis@post.jce.ac.il>
2012-12-15 20:35:19 +0200

    Refactor 24

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -8,7 +8,7 @@ public class RomanNumeralUnitTests {
 	private RomanNumerals number = new RomanNumerals();
 
 	@Test
-	public void testThat12ReturnsXII_passed() {
+	public void testThat24ReturnsXXIV_passed() {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
 		assertEquals("V", number.toRoman(5));
