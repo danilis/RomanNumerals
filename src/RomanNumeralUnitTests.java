7932e6bd165259e84f32f7eb7d68974ad8f85ae8
danilis <danilis@post.jce.ac.il>
2012-12-15 20:27:21 +0200

    Red Test V

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -11,7 +11,19 @@ public class RomanNumeralUnitTests {
 	public void testThat2ReturnsII_passed() {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
-		
+		assertEquals("IV", number.toRoman(5));
+		//assertEquals("VIII", number.toRoman(8));
+		//assertEquals("XII", number.toRoman(12));
+		//assertEquals("XXIV", number.toRoman(24));
+		//assertEquals("XLIX", number.toRoman(49));
+		//assertEquals("LXXVII", number.toRoman(77));
+		//assertEquals("XCIX", number.toRoman(99));
+		//assertEquals("CCLIV", number.toRoman(254));
+		//assertEquals("DCCLXVIII", number.toRoman(768));
+		//assertEquals("MI", number.toRoman(1001));
+		//assertEquals("MDLXVII", number.toRoman(1567));
+		//assertEquals("MMCCCXXXIII", number.toRoman(2333));
+		//assertEquals("MMMCMXCIX", number.toRoman(3999));
 	}
 	
 
