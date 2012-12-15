6221079fe5d638c1e155012ef6122863b5aec685
danilis <danilis@post.jce.ac.il>
2012-12-15 20:51:37 +0200

    Refactor test 3999

--- src/RomanNumeralUnitTests.java
+++ src/RomanNumeralUnitTests.java
@@ -8,7 +8,7 @@ public class RomanNumeralUnitTests {
 	private RomanNumerals number = new RomanNumerals();
 
 	@Test
-	public void testThat2333ReturnsMMCCCXXXIII_passed() {
+	public void testThat3999ReturnsMMMCMXCIX_passed() {
 		assertEquals("I", number.toRoman(1));
 		assertEquals("II", number.toRoman(2));
 		assertEquals("V", number.toRoman(5));
